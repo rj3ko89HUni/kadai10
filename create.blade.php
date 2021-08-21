13b3649759102078d2ef3eeaeabb9008bdd56501
rj3ko89HUni <takaki200109@gmail.com>
2021-08-21 07:07:14 +0000

    kadai10

---
+++ create.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF=8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewpport" content="width=device-width,initial-scale=1">
+        
+        <title>My Profile</title>
+    </head>
+    <body>
+        <h1>My Profile</h1>
+    </body>
+</html>
+
---
+++ edit.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF=8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewpport" content="width=device-width,initial-scale=1">
+        
+        <title>My Edit</title>
+    </head>
+    <body>
+        <h1>My Edit</h1>
+    </body>
+</html>
